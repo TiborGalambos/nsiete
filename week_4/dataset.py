16c16f37b2275f39a76d2f154ad8426479a08ea9
Lukas Hudec <hudec.lukas21@gmail.com>
2023-03-18 12:55:24 +0100

    Decision Boundary and Circles Dataset

--- week_4/dataset.py
+++ week_4/dataset.py
@@ -1,5 +1,36 @@
 import numpy as np
 import plotly.express as px
+import matplotlib.pyplot as plt
+
+def dataset_Circles(m=10, radius=0.7, noise=0.0, verbose=False):
+
+    # Hodnoty X budu v intervale <-1; 1>
+    X = (np.random.rand(2, m) * 2.0) - 1.0
+    if (verbose): print('X: \n', X, '\n')
+
+    # Element-wise nasobenie nahodnym sumom
+    N = (np.random.rand(2, m)-0.5) * noise
+    if (verbose): print('N: \n', N, '\n')
+    Xnoise = X + N
+    if (verbose): print('Xnoise: \n', Xnoise, '\n')
+
+    # Spocitame polomer
+    # Element-wise druha mocnina
+    XSquare = Xnoise ** 2
+    if (verbose): print('XSquare: \n', XSquare, '\n')
+
+    # Spocitame podla prvej osi. Ziskame (1, m) array.
+    RSquare = np.sum(XSquare, axis=0, keepdims=True)
+    if (verbose): print('RSquare: \n', RSquare, '\n')
+    R = np.sqrt(RSquare)
+    if (verbose): print('R: \n', R, '\n')
+
+    # Y bude 1, ak je polomer vacsi ako argument radius
+    Y = (R > radius).astype(float)
+    if (verbose): print('Y: \n', Y, '\n')
+
+    # Vratime X, Y
+    return X, Y
 
 def dataset_Flower(m=10, noise=0.0):
     # Inicializujeme matice
@@ -58,6 +89,41 @@ def MakeBatches(dataset, batchSize, shuffle:bool):
         result.append((np.expand_dims(minibatchX, axis=-1), np.expand_dims(minibatchY, axis=-1)))
 
     return result
+
+def draw_DecisionBoundary(X, Y, model, size=6):
+
+    # Najdeme hranice, pre ktore budeme skumat predikciu
+    pad = 0.5
+    x1_Min, x1_Max = X[0,:].min()-pad, X[0,:].max()+pad
+    x2_Min, x2_Max = X[1,:].min()-pad, X[1,:].max()+pad
+
+    # Spravime mriezku dvojic - vzorkujeme cely interval <MIN; MAX> s granularitou h
+    h = 0.01  
+    x1_Grid, x2_Grid = np.meshgrid(
+        np.arange(x1_Min, x1_Max, h),
+        np.arange(x2_Min, x2_Max, h)
+    )
+
+    # Usporiadame si mriezku hodnot do rovnakeho tvaru ako ma X
+    XX = np.c_[x1_Grid.ravel(), x2_Grid.ravel()].T
+
+    # Vypocitame predikciu pomocou modelu na vsetky hodnoty mriezky
+    YHat = model(XX)
+
+    # A usporiadame si vysledok tak, aby sme ho mohli podhodit PyPlotu
+    YHat = YHat.reshape(x1_Grid.shape)
+
+    # Najskor nakreslime contour graf - vysledky skumania pre mriezku
+    plt.figure(figsize=(size, size))
+    plt.xscale('linear')
+    plt.yscale('linear')
+    plt.contourf(x1_Grid, x2_Grid, YHat, cmap=plt.cm.RdYlBu)
+
+    # Potom este pridame scatter graf pre X, Y
+    plt.scatter(X[0,:], X[1,:], c=Y, cmap=plt.cm.RdBu)
+
+    plt.show()
+    plt.close()
 
 
 def draw_dataset(x, y):
